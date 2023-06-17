f389ff4f0b1cca0b7ab125730fa2ba5a4c47f928
simi <alchemy.officialz@gmail.com>
2023-06-17 17:00:40 +0530

    second commit

--- main.js
+++ main.js
@@ -7,13 +7,82 @@ function addItem() {
   let itemEnter = kitchenInput.value;
   console.log(itemEnter);
   let listCreate = document.createElement("li");
+  listCreate.style.marginBottom = '10px';
+  listCreate.style.backgroundColor = 'aqua';
+  listCreate.classList.add('trash')
   listCreate.innerText = itemEnter;
   kitchenList.appendChild(listCreate);
   console.log(listCreate);
   kitchenInput.value = ""
   kitchenInput.focus();
+
+  //create i tag for trash button
+  let trashButton = document.createElement('i');
+  // trashButton.style.
+  trashButton.classList.add('fas', 'fa-trash');
+  listCreate.appendChild(trashButton)
+  // console.log(trashButton);
+  // trashButton.addEventListener('click',deleteItem);
+
+}
+const deleteItem=()=>{
+alert('delete')
 }
 
-
 addButton.addEventListener("click", addItem);
+kitchenList.addEventListener('click',deleteItem);
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------
+
+// document.addEventListener('DOMContentLoaded', () => {
+// let id1 = document.getElementById("id1");
+// let id2 = document.getElementById("id2");
+// let id3 = document.getElementById("id3");
+
+// const display = () => {
+//   let inputValue = id1.value;
+//   let listCreate1 = document.createElement('li');
+//   listCreate1.style.backgroundColor = 'green';
+//   listCreate1.style.marginTop= '10px';
+//   listCreate1.innerText = inputValue;
+//   id3.appendChild(listCreate1);
+//   id1.value = ''
+//   id1.focus();
+// }
+
+
+// id2.addEventListener('click', display);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
